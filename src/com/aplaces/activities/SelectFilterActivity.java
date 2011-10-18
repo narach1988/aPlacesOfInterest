4d796bac6b9fec175412c4f39bac3978c0f82779
narach1988 <snaralenkov@ruswizards.com>
2011-10-18 21:03:52 +0300

    Creation of the project

---
+++ src/com/aplaces/activities/SelectFilterActivity.java
@@ -0,0 +1,15 @@
+package com.aplaces.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectFilterActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
